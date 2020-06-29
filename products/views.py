4dd03cc550795145d6a20bba61aa1189cd6b6d26
Scrambles86 <craigvidler86@outlook.com>
2020-06-29 14:27:53 +0000

    Added sorting and product counts to products page

--- products/views.py
+++ products/views.py
@@ -9,10 +9,10 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    query= None
-    categories= None
-    sort= None
-    direction= None
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'sort' in request.GET:
@@ -20,19 +20,19 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = product.annotate(lower_name=Lower('name'))
-
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                if 'direction' == 'desc':
+                if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -47,9 +47,9 @@ def all_products(request):
 
     context = {
         'products': products,
-        'search-term': query,
+        'search_term': query,
         'current_categories': categories,
-        'current_sorting' : current_sorting,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
